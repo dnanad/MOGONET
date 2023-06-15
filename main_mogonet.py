425eb9177b022eecb15a734b96536308fea93fc9
danand <danand1101@gmail.com>
2023-06-15 16:30:17 +0200

    first commit

--- main_mogonet.py
+++ main_mogonet.py
@@ -5,8 +5,8 @@ from train_test import train_test
 if __name__ == "__main__":    
     data_folder = 'ROSMAP'
     view_list = [1,2,3]
-    num_epoch_pretrain = 500
-    num_epoch = 2500
+    num_epoch_pretrain = 100
+    num_epoch = 500
     lr_e_pretrain = 1e-3
     lr_e = 5e-4
     lr_c = 1e-3
