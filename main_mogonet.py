40a0185a3dc8c3e76a1455b384cd97d97c936504
danand <danand1101@gmail.com>
2023-07-10 09:31:20 +0200

    added save_model_dict

--- main_mogonet.py
+++ main_mogonet.py
@@ -1,21 +1,32 @@
 """ Example for MOGONET classification
 """
 from train_test import train_test
+from models import save_model
+from utils import save_model_dict
+import os
 
 if __name__ == "__main__":
     data_folder = "TEST_DATA"  # "ROSMAP"
-    view_list = [1]  # [1,2,3]
-    num_epoch_pretrain = 100
-    num_epoch = 500
-    test_interval = 50
+    model_folder = os.path.join(data_folder, "models")
+    # os.makedirs(model_folder) if not os.path.exists(model_folder) else print(
+    #     "Overwriting contents in existing dir"
+    # )
+    rootpath = os.path.dirname(os.path.realpath(__file__))
+    model_folder_path = os.path.join(rootpath, model_folder)
+
+    view_list = [1]  # [1, 2, 3]  #  [1]  # [1,2,3]
+    num_view = len(view_list)
+    num_epoch_pretrain = 10
+    num_epoch = 50
+    test_interval = 5
     lr_e_pretrain = 1e-3
     lr_e = 5e-4
     lr_c = 1e-3
 
     if data_folder == "TEST_DATA":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
+        num_class = 2  # number of classes
+        adj_parameter = 2  # number of neighbors for each node
+        dim_he_list = [400, 400, 200]  # hidden dimensions for each view
     if data_folder == "ROSMAP":
         num_class = 2
         adj_parameter = 2
@@ -25,7 +36,7 @@ if __name__ == "__main__":
         adj_parameter = 10
         dim_he_list = [400, 400, 200]
 
-    train_test(
+    model_dict = train_test(
         data_folder,
         view_list,
         num_class,
@@ -38,3 +49,6 @@ if __name__ == "__main__":
         num_epoch,
         test_interval,
     )
+
+    # save the model
+    save_model_dict(model_folder_path, model_dict)
