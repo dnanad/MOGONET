bcd1e2b8db85e5a204246b40d606f092e97b7bcd
danand <danand1101@gmail.com>
2023-08-03 17:35:34 +0200

    model path changed

--- main_biomarker.py
+++ main_biomarker.py
@@ -3,18 +3,29 @@
 import os
 import copy
 from feat_importance import cal_feat_imp, summarize_imp_feat
+from utils import find_numFolders_maxNumFolders
 
 if __name__ == "__main__":
-    data_folder = "TEST_DATA"  # "ROSMAP" #"BRCA
+    data_folder = "PE_cfRNA"  # "TEST_DATA_copy"  # "ROSMAP" #"BRCA
     model_folder = os.path.join(data_folder, "models")
-    view_list = [1]  # [1, 2, 3]
-    # if data_folder == "TEST_DATA":
-    #     num_class = 2
-    # if data_folder == "ROSMAP":
-    #     num_class = 2
-    # if data_folder == "BRCA":
-    #     num_class = 5
 
+    rootpath = os.path.dirname(os.path.realpath(__file__))
+    data_folder_path = os.path.join(rootpath, data_folder)
+    model_folder_path = os.path.join(rootpath, model_folder)
+
+    view_list, _ = find_numFolders_maxNumFolders(data_folder_path)
+    # view_list = [1]
+    num_view = len(view_list)
+    topn = 30
+
+    if data_folder == "PE_cfRNA":
+        num_class = 2
+        adj_parameter = 2
+        dim_he_list = [400, 400, 200]
+    if data_folder == "TEST_DATA_copy":
+        num_class = 2  # number of classes
+        adj_parameter = 2  # number of neighbors for each node
+        dim_he_list = [400, 400, 200]  # hidden dimensions for each view
     if data_folder == "TEST_DATA":
         num_class = 2  # number of classes
         adj_parameter = 2  # number of neighbors for each node
@@ -35,8 +46,8 @@ if __name__ == "__main__":
     #     )
     #     featimp_list_list.append(copy.deepcopy(featimp_list))
     featimp_list = cal_feat_imp(
-        data_folder, model_folder, view_list, num_class, adj_parameter, dim_he_list
+        data_folder, model_folder_path, view_list, num_class, adj_parameter, dim_he_list
     )
     featimp_list_list.append(copy.deepcopy(featimp_list))
 
-    summarize_imp_feat(featimp_list_list)
+    summarize_imp_feat(featimp_list_list, topn=topn)
