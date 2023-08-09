495dbc586a7428256c1e754bcaca3b3d08b6b962
Anand Deshpande <anand@Anands-MBP.fritz.box>
2023-08-09 17:28:23 +0200

    added path to the exp

--- main_biomarker.py
+++ main_biomarker.py
@@ -6,18 +6,23 @@ from feat_importance import cal_feat_imp, summarize_imp_feat
 from utils import find_numFolders_maxNumFolders
 
 if __name__ == "__main__":
-    data_folder = "PE_cfRNA"  # "TEST_DATA_copy"  # "ROSMAP" #"BRCA
+    data_folder = "PE_cfRNA"  # "PE_cfRNA_pre"  # "TEST_DATA_copy"  # "ROSMAP" #"BRCA
     model_folder = os.path.join(data_folder, "models")
-
+    exp_name = "500_2500_20230809-144552"
     rootpath = os.path.dirname(os.path.realpath(__file__))
     data_folder_path = os.path.join(rootpath, data_folder)
     model_folder_path = os.path.join(rootpath, model_folder)
+    exp_path = os.path.join(model_folder_path, exp_name)
 
     view_list, _ = find_numFolders_maxNumFolders(data_folder_path)
     # view_list = [1]
     num_view = len(view_list)
     topn = 30
 
+    if data_folder == "PE_cfRNA_pre":
+        num_class = 2
+        adj_parameter = 2
+        dim_he_list = [400, 400, 200]
     if data_folder == "PE_cfRNA":
         num_class = 2
         adj_parameter = 2
@@ -46,8 +51,8 @@ if __name__ == "__main__":
     #     )
     #     featimp_list_list.append(copy.deepcopy(featimp_list))
     featimp_list = cal_feat_imp(
-        data_folder, model_folder_path, view_list, num_class, adj_parameter, dim_he_list
+        data_folder, exp_path, view_list, num_class, adj_parameter, dim_he_list
     )
     featimp_list_list.append(copy.deepcopy(featimp_list))
 
-    summarize_imp_feat(featimp_list_list, topn=topn)
+    summarize_imp_feat(featimp_list_list, exp_path, topn=topn)
